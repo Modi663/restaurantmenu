5933351c24b9e7af62c487c8b773d72047766ee6
vadim <berbeca.vadik67@gmail.com>
2024-06-07 07:19:43 +0300

    Init project

---
+++ src/main/java/com/example/restaurantmenu/RestaurantmenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantmenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantmenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restaurantmenu/RestaurantmenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantmenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
